6777f7c65f99d666728c0eae56c6500ae5056f01
Nick <nsagias@gmail.com>
2023-03-16 11:29:16 -0400

    shrink-0

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -1,7 +1,18 @@
 export default function GridLayout() {
   return (
     <>
-    <div className="bg-yellow-300 min-h-full min-w-full flex items-center justify-center p-5">
+    <div className="bg-yellow-300 min-h-full min-w-full flex items-center justify-center p-5 shrink-0">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-x-4 gap-y-4 min-h-full min-w-full">
+        <div className="col-span-1 md:col-span-2 lg:row-span-2 bg-black p-3 rounded text-white h-screen w-full box">1</div>
+        <div className="bg-black p-3 rounded text-white box">2</div>
+        <div className="bg-black p-3 rounded text-white box">3</div>
+        <div className="bg-black p-3 rounded text-white box">4</div>
+        <div className="bg-black p-3 rounded text-white box">5</div>
+        <div className="bg-black p-3 rounded text-white box">6</div>
+        <div className="bg-black p-3 rounded text-white box">7</div>
+      </div>
+    </div>
+    {/* <div className="bg-yellow-300 min-h-full min-w-full flex items-center justify-center p-5">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-x-1 gap-y-2 min-h-full min-w-full">
         <div className="col-span-1 md:col-span-2 lg:row-span-2 bg-black p-3 rounded text-white h-full w-full box">1</div>
         <div className="bg-black p-3 rounded text-white box">2</div>
@@ -12,19 +23,7 @@ export default function GridLayout() {
         <div className="bg-black p-3 rounded text-white box">7</div>
       </div>
       
-    </div>
-    <div className="bg-yellow-300 min-h-full min-w-full flex items-center justify-center p-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-x-1 gap-y-2 min-h-full min-w-full">
-        <div className="col-span-1 md:col-span-2 lg:row-span-2 bg-black p-3 rounded text-white h-full w-full box">1</div>
-        <div className="bg-black p-3 rounded text-white box">2</div>
-        <div className="bg-black p-3 rounded text-white box">3</div>
-        <div className="bg-black p-3 rounded text-white box">4</div>
-        <div className="bg-black p-3 rounded text-white box">5</div>
-        <div className="bg-black p-3 rounded text-white box">6</div>
-        <div className="bg-black p-3 rounded text-white box">7</div>
-      </div>
-      
-    </div>
+    </div> */}
     </>
   );
 }
