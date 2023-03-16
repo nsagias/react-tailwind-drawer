29812d4d244448562fc746108dfe91a522f88647
Nick <nsagias@gmail.com>
2023-03-16 00:16:35 -0400

    add response font sizes and spacing

--- src/components/ResponsiveGrid.jsx
+++ src/components/ResponsiveGrid.jsx
@@ -2,10 +2,10 @@ export default function ResponsiveGrid() {
   return (
     <>
      <div className="min-h-screen bg-yellow-300 p-10">
-      <div className="space-y-10">
+      <div className="space-y-10 md:space-y-0 md:grid md:grid-cols-2">
 
-        <div className="">
-          <h2 className="text-black text-4xl font-bold mb-4">Responsive</h2>
+        <div className="md:flex md:flex-col md:justify-center">
+          <h2 className="text-black text-2xl md:text-4xl lg:text-6xl font-bold mb-4">Responsive</h2>
           <p className="text-lg text-gray-900">Responsive Tailwind</p>
         </div>
         <div className="">
