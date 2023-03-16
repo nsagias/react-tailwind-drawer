062e104b9a8d2c1d248b955f27aa67f5bdbe705e
Nick <nsagias@gmail.com>
2023-03-16 02:11:30 -0400

    maximize size

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -2,7 +2,7 @@ export default function GridLayout() {
   return (
     <div className="bg-yellow-300 min-h-screen flex items-center justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-x-1 gap-y-2">
-        <div className="col-span-1 md:col-span-2 lg:row-span-2 bg-black p-3 rounded text-white large_box">1</div>
+        <div className="col-span-1 md:col-span-2 lg:row-span-2 bg-black p-3 rounded text-white w-full h-full shadow-2xl">1</div>
         <div className="bg-black p-3 rounded text-white box">2</div>
         <div className="bg-black p-3 rounded text-white box">3</div>
         <div className="bg-black p-3 rounded text-white box">4</div>
