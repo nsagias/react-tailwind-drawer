b7250cf0edab7e444971bdcf3755c3a0a1988d2e
Nick <nsagias@gmail.com>
2023-03-16 00:07:41 -0400

    add responsive grid component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Drawer from './components/Drawer';
+import ResponsiveGrid from './components/ResponsiveGrid';
+
 
 export default function App() {
   return (
-    <div className="mx-auto mt-8">
+    <div className="mx-auto lg:mt-8">
       <Drawer />
-      <h1 className="text-4xl text-center">A Page</h1>
+      {/* <h1 className="text-4xl text-center">A Page</h1> */}
+      <ResponsiveGrid />
     </div>
   );
 }
---
+++ src/components/ResponsiveGrid.jsx
@@ -0,0 +1,19 @@
+export default function ResponsiveGrid() {
+  return (
+    <>
+     <div className="min-h-screen bg-yellow-300 p-10">
+      <div className="space-y-10">
+
+        <div className="">
+          <h2 className="text-black text-4xl font-bold mb-4">Responsive</h2>
+          <p className="text-lg text-gray-900">Responsive Tailwind</p>
+        </div>
+        <div className="">
+          <div className="w-full h-64 rounded-lg shadow-2xl bg-black"></div>
+        </div>
+
+      </div>
+    </div>
+    </>
+  );
+}
