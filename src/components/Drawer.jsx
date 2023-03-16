556712b9fe155515245fb0dbcf6a82342e5559cc
Nick <nsagias@gmail.com>
2023-03-15 21:09:26 -0400

    install react-cons

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import {   RxHamburgerMenu } from "react-icons/rx";
+
 export default function Drawer() {
+
+  const [isDrawerVisible, setDrawerVisible] = useState(false);
+
   return (
-    <div className="top-0 right-0 fixed bg-blue-400 text-white w-[35vw] h-screen p-10">
-      <h2>This is the drawer</h2>
-    </div>
+    <>
+    { !isDrawerVisible ? 
+      (
+        <>
+        <RxHamburgerMenu />
+        </>
+      ) : (
+        <div className="top-0 right-0 fixed bg-blue-400 text-white w-[35vw] h-screen p-10">
+          <h2>This is the drawer</h2>
+        </div>
+      )}
+    
+    </>
   );
 }
