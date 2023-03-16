545b2aa4ec09e22670e66f468910874d24c2c562
Nick <nsagias@gmail.com>
2023-03-16 03:00:39 -0400

    test double grid

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -1,5 +1,6 @@
 export default function GridLayout() {
   return (
+    <>
     <div className="bg-yellow-300 min-h-full min-w-full flex items-center justify-center p-5">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-x-1 gap-y-2 min-h-full min-w-full">
         <div className="col-span-1 md:col-span-2 lg:row-span-2 bg-black p-3 rounded text-white h-full w-full box">1</div>
@@ -10,6 +11,20 @@ export default function GridLayout() {
         <div className="bg-black p-3 rounded text-white box">6</div>
         <div className="bg-black p-3 rounded text-white box">7</div>
       </div>
+      
     </div>
+    <div className="bg-yellow-300 min-h-full min-w-full flex items-center justify-center p-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-x-1 gap-y-2 min-h-full min-w-full">
+        <div className="col-span-1 md:col-span-2 lg:row-span-2 bg-black p-3 rounded text-white h-full w-full box">1</div>
+        <div className="bg-black p-3 rounded text-white box">2</div>
+        <div className="bg-black p-3 rounded text-white box">3</div>
+        <div className="bg-black p-3 rounded text-white box">4</div>
+        <div className="bg-black p-3 rounded text-white box">5</div>
+        <div className="bg-black p-3 rounded text-white box">6</div>
+        <div className="bg-black p-3 rounded text-white box">7</div>
+      </div>
+      
+    </div>
+    </>
   );
 }
